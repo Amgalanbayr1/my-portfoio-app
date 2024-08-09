da6e6af0e11a942f7044e63cb9f6991cc36f251d
amgalanbayr.j <j.amgalanbayr1@gmail.com>
2024-08-09 17:00:30 +0800

    developing my portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+    },
+    compiler: {
+      styledComponents: true,
+    },
+  }
+  
+  export default nextConfig
+  
