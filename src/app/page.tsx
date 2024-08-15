1ad1b9a06c31c8ffcfbef17a7d2d94a35bde088f
amgalanbayr.j <j.amgalanbayr1@gmail.com>
2024-08-15 20:14:31 +0800

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center w-full p-0">
       <Hero />
-      <About />
       <br />
+      <About />
       <Projects />
       <Experience />
       <Skills />
